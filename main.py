e1c77b11214397d244057b7e439034a97a1ce0d9
Niclas Trelle <niclas.trelle@gmx.de>
2021-05-18 17:58:29 +0200

    added csv

--- main.py
+++ main.py
@@ -1,2 +1,47 @@
 import csv
+import sys
+
+#format of the csv file has to be: modification as double, cost per unit as double
+#at first, always the beginning state
+
+def main(arg):
+    if arg == "test":
+        test()
+    if arg == "perm_lifo":
+        permanent_lifo()
+
+def test():
+    with open("people.csv","r") as file:
+        reader = csv.reader(file)
+        list = []
+        for row in reader:
+            list.append(row)
+    print(list)
+
+def permanent_lifo():
+    #preprocessing
+    with open("people.csv","r") as file:
+        reader = csv.reader(file)
+        list = []
+        for row in reader:
+            list.append(row)
+
+    #processing
+    aktuell = int(list[0][0])*int(list[0][1])
+    i = 1
+    print(len(list))
+    while i <= len(list):
+        #Wenn Abgang
+        print(i)
+        i += 1
+    print("test")
+
+if __name__ == "__main__":
+    try:
+        main(sys.argv[1])
+    except:
+        print("no arguments found - select a procedure") #add -h
+    main(sys.argv[1])
+    pass
+
 
