779cdc1698b11488768f5efa5a4f81c4cc58b02c
Niclas Trelle <niclas.trelle@gmx.de>
2021-05-18 19:03:16 +0200

    added buggy permanent_fifo

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ import sys
 def main(arg):
     if arg == "test":
         test()
-    if arg == "perm_lifo":
-        permanent_lifo()
+    if arg == "perm_fifo":
+        permanent_fifo()
 
 def test():
     with open("people.csv","r") as file:
@@ -18,7 +18,7 @@ def test():
             list.append(row)
     print(list)
 
-def permanent_lifo():
+def permanent_fifo():
     #preprocessing
     with open("people.csv","r") as file:
         reader = csv.reader(file)
@@ -27,21 +27,41 @@ def permanent_lifo():
             list.append(row)
 
     #processing
-    aktuell = int(list[0][0])*int(list[0][1])
+    #aktuell = int(list[0][0])*int(list[0][1])
     i = 1
-    print(len(list))
-    while i <= len(list):
+    while i < len(list):
         #Wenn Abgang
-        print(i)
-        i += 1
-    print("test")
+        if int(list[i][0])<0:
+            while int(list[i][0])<0:
+                spot = find(list)
+                if int(list[spot][0])<int(list[i][0]):
+                    list[spot][0] = "0"
+                    list[i][0] = str(int(list[spot][0])+int(list[i][0]))
+                else:
+                    list[spot][0]=str(int(list[spot][0])+int(list[i][0]))
+                    list[i][0]="0"
 
+
+        i += 1
+    print(list)
+
+def find(list):
+    i = 0
+    for a in list:
+        if int(a[0]) > 0:
+            return i
+        i+=1
+
+permanent_fifo()
 if __name__ == "__main__":
     try:
         main(sys.argv[1])
     except:
         print("no arguments found - select a procedure") #add -h
-    main(sys.argv[1])
     pass
+
+
+
+
 
 
