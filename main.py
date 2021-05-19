b02f57faeabc302915c51f470237dbb7d5337de7
Niclas Trelle <niclas.trelle@gmx.de>
2021-05-19 19:51:54 +0200

    added periodic fifo

--- main.py
+++ main.py
@@ -29,9 +29,37 @@ def periodic_fifo():
         list = []
         for row in reader:
             list.append(row)
+    #processing
+    end = 0 #Endbestand
+    for a in list:
+        end = end + float(a[0])
+    print("Endbestand: " + str(end))
 
+    #last index is still in the depot when the rest gets picked first, which means the last prize has to be taken.
+    min = len(list)-1
+    bew_end = 0
+    while end > 0:
+        min = find_lifo(list,min)
+        if float(list[min][0]) > end:
+            bew_end = bew_end + end * float(list[min][1])
+            end = end - end
+        else:
+            bew_end = bew_end + abs(float(list[min][0]))*float(list[min][1])
+            end = end - abs(float(list[min][0]))
+        min -= 1
 
+    bew_mat = 0
+    outs = 0
+    for a in list:
+        if float(a[0])>0:
+            bew_mat = bew_mat + float(a[0])*float(a[1])
+        else:
+            outs = outs + abs(float(a[0]))
 
+    mat_cost = bew_mat - bew_end
+    av_cost = mat_cost/outs
+    print(list)
+    print("bewerteter Endbestand: {}\nbewertete Zugänge: {}\nKosten durch Abgänge: {} \ndurchschnittliche Kosten: {:.2f} euro/unit".format(bew_end,bew_mat,mat_cost,av_cost))
 
 def permanent_fifo():
     #preprocessing
@@ -42,8 +70,8 @@ def permanent_fifo():
             list.append(row)
 
     #processing
-    #aktuell = int(list[0][0])*int(list[0][1])
     i = 1
+    used_material = 0
     while i < len(list):
         #Wenn Abgang
         if float(list[i][0])<0:
@@ -52,16 +80,18 @@ def permanent_fifo():
                 if spot==None:
                     break
                 if float(list[spot][0])<abs(float(list[i][0])):
+                    used_material = used_material + abs(float(list[spot][0])) * float(list[spot][1])
                     list[i][0] = str(float(list[spot][0])+float(list[i][0]))
                     list[spot][0] = "0"
                 else:
+                    used_material = used_material + abs(float(list[i][0])) * float(list[spot][1])
                     list[spot][0]=str(float(list[spot][0])+float(list[i][0]))
                     list[i][0]="0"
             #Wenn Schritte erwünscht: Hier auskommentieren
             #print(list)
         i += 1
     print(list)
-    print(calculate(list))
+    print("bewerteter Endbestand: {}\nbewerteter Materialverbrauch: {}".format(calculate(list), used_material))
 
 def permanent_lifo():
     #preprocessing
@@ -72,26 +102,30 @@ def permanent_lifo():
             list.append(row)
 
     #processing
-    #aktuell = int(list[0][0])*int(list[0][1])
     i = 1
+    used_material = 0
     while i < len(list):
         #Wenn Abgang
         if float(list[i][0])<0:
             while float(list[i][0])<0:
-                spot = find_lifo(list)
+                spot = find_lifo(list, i)
                 if spot==None:
                     break
                 if float(list[spot][0])<abs(float(list[i][0])):
+                    used_material = used_material + abs(float(list[spot][0])) * float(list[spot][1])
                     list[i][0] = str(float(list[spot][0])+float(list[i][0]))
                     list[spot][0] = "0"
                 else:
+                    used_material = used_material + abs(float(list[i][0])) * float(list[spot][1])
                     list[spot][0]=str(float(list[spot][0])+float(list[i][0]))
                     list[i][0]="0"
             #Wenn Schritte erwünscht: Hier auskommentieren
             #print(list)
         i += 1
+    #process amount of used material
     print(list)
-    print(calculate(list))
+    print("bewerteter Endbestand: {}\nbewerteter Materialverbrauch: {}".format(calculate(list),used_material))
+    #print(calculate(list))
 
 def find_fifo(list):
     i = 0
@@ -100,10 +134,11 @@ def find_fifo(list):
             return i
         i+=1
 
-def find_lifo(list):
-    i = len(list)-1
-    for a in reversed(list):
-        if float(a[0]) > 0:
+def find_lifo(list, min):
+    i = min
+    #list = reversed(list)
+    while i>=0:
+        if float(list[i][0]) > 0:
             return i
         i-=1
 
@@ -114,7 +149,7 @@ def calculate(list):
             result = result + float(a[0])*float(a[1])
     return result
 
-permanent_lifo()
+periodic_fifo()
 if __name__ == "__main__":
     try:
         main(sys.argv[1])
